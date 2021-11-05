f99fdfbeaf61ed807ab9accc9580792d66835ff3
Bent <bent@mail.de>
2021-11-05 19:01:20 +0100

    Backend: Provide word status info

--- addon/__init__.py
+++ addon/__init__.py
@@ -182,7 +182,7 @@ aqt.gui_hooks.browser_menus_did_init.append(setup_browser_menu)
 
 
 def note_added(col, note, deck_id):
-    aqt.mw.migaku_kanji_db.on_note_update(note.id, deck_id)
+    aqt.mw.migaku_kanji_db.on_note_update(note.id, deck_id, is_new=True)
 
 add_note_no_hook = anki.collection.Collection.add_note
 anki.collection.Collection.add_note = anki.hooks.wrap(
