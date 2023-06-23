7a3347b64091c736a5a5c7b17346628bcdd8ae01
Henrik Giesel <hengiesel@gmail.com>
2023-06-24 01:05:08 +0200

    Refactor kanji confirm dialog

--- addon/kanji_confirm_dialog.py
+++ addon/kanji_confirm_dialog.py
@@ -30,21 +30,21 @@ class KanjiMarkModel(QAbstractListModel):
     def __init__(self):
         super().__init__()
         self.kanji = []
-        self.states = defaultdict(lambda: 0)
+        self.states: defaultdict[str, int] = defaultdict(lambda: 0)
 
-    def add(self, kanji):
-        new_kanji = []
-        for k in kanji:
-            if k not in self.kanji:
-                new_kanji.append(k)
-        if len(new_kanji) > 0:
-            self.beginInsertRows(
-                QModelIndex(),
-                len(self.kanji),
-                len(self.kanji) + len(new_kanji) - 1,
-            )
-            self.kanji.extend(new_kanji)
-            self.endInsertRows()
+    def add(self, kanji_list: list[str]):
+        new_kanji = [kanji for kanji in kanji_list if kanji not in self.kanji]
+
+        if len(new_kanji) == 0:
+            return
+
+        self.beginInsertRows(
+            QModelIndex(),
+            len(self.kanji),
+            len(self.kanji) + len(new_kanji) - 1,
+        )
+        self.kanji.extend(new_kanji)
+        self.endInsertRows()
 
     def data(self, idx, role):
         if not idx.isValid():
@@ -59,26 +59,22 @@ class KanjiMarkModel(QAbstractListModel):
             return QColor(self.state_colors[state])
         return QVariant()
 
-    def rowCount(self, parent):
+    def rowCount(self, _parent):
         return len(self.kanji)
 
     def cycle(self, idx):
         if idx.isValid():
-            index = idx.row()
-            kanji = self.kanji[index]
+            kanji = self.kanji[idx.row()]
             state = self.states[kanji]
             state += 1
             if state > 2:
                 state = 0
-            self.states[kanji] = state
-            self.dataChanged.emit(idx, idx)
 
-    def with_state(self, state):
-        ret = []
-        for k in self.kanji:
-            if self.states[k] == state:
-                ret.append(k)
-        return ret
+            self.states[kanji] = state
+            self.dataChanged.emit(idx, idx, [Qt.ItemDataRole.BackgroundRole])
+
+    def with_state(self, state: int):
+        return [kanji for kanji in self.kanji if self.states[kanji] == state]
 
     def to_add(self):
         return self.with_state(0)
@@ -148,25 +144,25 @@ class KanjiConfirmDialog(QDialog):
         info_lbl.setWordWrap(True)
         lyt.addWidget(info_lbl)
 
-        self.ct_widgets = {}
-        self.ct_labels = {}
+        self.card_type_widgets = {}
+        self.card_type_labels = {}
 
         for card_type in CardType:
             card_type_label = QLabel(card_type.name)
             card_type_label.setHidden(True)
             lyt.addWidget(card_type_label)
-            self.ct_labels[card_type] = card_type_label
+            self.card_type_labels[card_type] = card_type_label
 
             ct_kmw = KanjiMarkWidget()
             ct_kmw.setHidden(True)
             lyt.addWidget(ct_kmw)
-            self.ct_widgets[card_type] = ct_kmw
+            self.card_type_widgets[card_type] = ct_kmw
 
         self.resize(500, 400)
 
-        btn_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
-        btn_box.accepted.connect(self.accept)
-        lyt.addWidget(btn_box)
+        button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+        button_box.accepted.connect(self.accept)
+        lyt.addWidget(button_box)
 
         self.add_kanji(ct_kanji)
 
@@ -176,17 +172,17 @@ class KanjiConfirmDialog(QDialog):
 
     def add_kanji(self, ct_kanji):
         for ct in ct_kanji:
-            ct_kmw = self.ct_widgets[ct]
+            ct_kmw = self.card_type_widgets[ct]
             kanji = ct_kanji[ct]
             ct_kmw.add(kanji)
             ct_kmw.setHidden(False)
-            self.ct_labels[ct].setHidden(False)
+            self.card_type_labels[ct].setHidden(False)
 
     def accept(self):
         database = aqt.mw.migaku_kanji_db
 
-        for ct in self.ct_widgets:
-            ctw = self.ct_widgets[ct]
+        for ct in self.card_type_widgets:
+            ctw = self.card_type_widgets[ct]
             to_add = ctw.to_add()
 
             if len(to_add) < 1:
@@ -207,24 +203,24 @@ class KanjiConfirmDialog(QDialog):
         super().accept()
 
     def reject(self):
-        r = QMessageBox.question(
+        response = QMessageBox.question(
             self,
             "Migaku Kanji",
             "Do you really want to ignore these Kanji? No kanji cards will be created and none will be marked known.",
         )
-        if r == QMessageBox.StandardButton.Yes:
+        if response == QMessageBox.StandardButton.Yes:
             self.on_close()
             super().reject()
 
     @classmethod
-    def _show_new_kanji(cls, ct_kanji, parent=None):
+    def _show_new_kanji(cls, card_type_kanji, parent=None):
         if cls.instance is None:
-            cls.instance = cls(parent, ct_kanji)
+            cls.instance = cls(parent, card_type_kanji)
             cls.instance.show()
         else:
-            cls.instance.add_kanji(ct_kanji)
+            cls.instance.add_kanji(card_type_kanji)
             util.raise_window(cls.instance)
 
     @classmethod
-    def show_new_kanji(cls, ct_kanji, parent=None):
-        aqt.mw.taskman.run_on_main(lambda: cls._show_new_kanji(ct_kanji, parent))
+    def show_new_kanji(cls, card_type_kanji, parent=None):
+        aqt.mw.taskman.run_on_main(lambda: cls._show_new_kanji(card_type_kanji, parent))
