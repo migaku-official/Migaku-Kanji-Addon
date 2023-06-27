b4625c231e1bd19585c41cff05994f4f69bdfa26
Henrik Giesel <hengiesel@gmail.com>
2023-06-27 14:16:17 +0200

    Reformatting via black

--- tools/kanjivg_cleanup.py
+++ tools/kanjivg_cleanup.py
@@ -1,21 +1,17 @@
 import os
 
 kanjivg_dir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    '..',
-    'addon',
-    'kanjivg'
+    os.path.dirname(os.path.abspath(__file__)), "..", "addon", "kanjivg"
 )
 
 for fname in os.listdir(kanjivg_dir):
-
-    if fname.endswith('.svg'):
-        with open(os.path.join(kanjivg_dir, fname), 'r') as f:
+    if fname.endswith(".svg"):
+        with open(os.path.join(kanjivg_dir, fname), "r") as f:
             svg = f.read()
 
-        idx = svg.find('<svg')
+        idx = svg.find("<svg")
         if idx > 0:
             svg = svg[idx:]
 
-        with open(os.path.join(kanjivg_dir, fname), 'w') as f:
+        with open(os.path.join(kanjivg_dir, fname), "w") as f:
             f.write(svg)
