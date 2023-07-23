4f9984c25e9b1001bb84bfb71cb5fab560cb1b67
mjuhanne <marko.juhanne@gmail.com>
2023-07-24 00:37:04 +0300

    Major update to kanji database, trying to replicate Heisig's keywords and primitives as accurately as possible: - Add all the missing non-Unicode character primitives - Fix many mix-ups between primitives ('good luck' vs 'lidded crock',  'chop-seal' vs 'stamps' among others) - separate 'hand' and 'fingers' primitives for clarity. - primitives for 'flowers', 'city walls' and 'pinnacle' actually reference the more common radicals instead of weird arcaic ones - Fix many erroneous primitive references (such as 'water' being used instead of 'rice grains') - Fix many Heisig stories and comments. Also add italic and bold text when referencing keywords
    Changes to the original kanji database (as of 7/2023) are read from kanji-ext.tsv. Included is the updated kanji.db.  Log of changes can be found in db_merge_log.md in Markdown format

--- tools/db_merge_tsv.py
+++ tools/db_merge_tsv.py
@@ -1,19 +1,37 @@
+#
+# Merges changed kanjis/primitives listed in kanji-ext.tsv with kanji.db
+# Recalculates also primitives-of list
+#
 import sqlite3
 import json
 import sys
 import os
 import re
+import logging
 
 # Creates a list of single-character Unicode kanjis and [primitive] tags
 # For example '[banner]也' -> ['\[banner\]','也'] 
 def custom_list(l):
-    g = re.findall(r'([^\[]|\[.+\])',l)
+    g = re.findall(r'([^\[]|\[[^\]]+\])',l)
     return g
 
-ext_tsv_path = sys.argv[1] if len(sys.argv) > 1 else "kanji-ext.tsv"
-db_path = sys.argv[2] if len(sys.argv) > 2 else "kanji.db"
 
+# create multi-line string for better readibility in markdown tables
+def multiLine(src_list,n):    
+    chunks = [src_list[i:i+n] for i in range(0, len(src_list), n)]
+    lines = [ ''.join(chunk) for chunk in chunks ]            
+    return '<br>'.join(lines)
+
+
+ext_tsv_path = sys.argv[1] if len(sys.argv) > 1 else "addon/kanji-ext.tsv"
+db_path = sys.argv[2] if len(sys.argv) > 2 else "addon/kanji.db"
+log_path = sys.argv[3] if len(sys.argv) > 3 else "db_merge_log.md"
 db_path = os.path.abspath(db_path)
+
+### set up logging
+targets = logging.StreamHandler(sys.stdout), logging.FileHandler(log_path,'w+')
+logging.basicConfig(format='%(message)s', level=logging.INFO, handlers=targets)
+
 
 con = sqlite3.connect(db_path)
 #con.row_factory = sqlite3.Row
@@ -44,15 +62,16 @@ update_prim_of_sql = (
     f'UPDATE characters SET primitive_of=? WHERE character=?'
 )
 
-def to_json_list_str(d):
-    if d!= '':
-        return json.dumps(d.replace(' ','').split(','))
+def to_json_list_str(csv):
+    if csv!= '':
+        item_list = csv.split(',')
+        clean_list = [item.strip() for item in item_list]
+        return json.dumps(clean_list)
     return '[]'
 
-def to_list_str(d):
-    if d!= '':
-        return str( d.replace(' ','').split(',') )
-    return '[]'
+def j2c(d):
+    return ", ".join(json.loads(d))
+
 
 # (field_name, load_function, column)
 _ = lambda x: x
@@ -66,22 +85,68 @@ field_conversion = [
     (6,"primitive_keywords", to_json_list_str, None),
     (7,"heisig_story", _, None),
     (8,"heisig_comment", _, None),
-    (9,"radicals", to_list_str, None),
+    (9,"radicals", _, None),
+]
+
+field_conversion_reverse = [
+    (0,"character", _, None),
+    (1,"meanings", j2c, None),
+    (2,"primitive_alternatives", _, None),
+    (3,"primitives", _, None),
+    (4,"heisig_keyword5", _, None),
+    (5,"heisig_keyword6", _, None),
+    (6,"primitive_keywords", j2c, None),
+    (7,"heisig_story", _, None),
+    (8,"heisig_comment", _, None),
+    (9,"radicals", _, None),
 ]
 
 
+
+processed_kanji_list = []
+total_changes = 0
+
 for l in open(ext_tsv_path, "r", encoding="utf-8"):
+    
     d = l.replace("\n", "").split("\t")
+    if len(d[0]) == 0:
+        logging.info("")
+        continue
     if d[0] == 'Kanji':  # omit the header
+        continue
+    if d[0][0]=='#':  # only log the comments
+        # modify the comments so they look better in markdown
+        hash_count = d[0].count('#')
+        cleaned_comment = d[0].replace('#', '')
+        if hash_count > 5:
+            logging.info('----')
+            comment = '## ' + cleaned_comment
+        elif hash_count > 1:
+            comment = '###' + cleaned_comment
+        else:
+            comment = '####' + cleaned_comment
+        logging.info(comment)
         continue
 
     if len(d) != 10:
-        print("Error! Wrong length in data: ",d)
-        continue
+        raise Exception("Error! Wrong length in data: %s" % str(d))
 
     kanji = d[0].strip()
 
-    print("Processing", kanji)
+    if kanji in processed_kanji_list:
+        raise Exception("Kanji %s already processed! Remove duplicate" % kanji)
+    
+    processed_kanji_list.append(kanji)
+
+    # create header
+    pretty_header = kanji
+    if d[4] != '':
+        pretty_header += ' ' + d[6]
+        if d[6] != '':
+            pretty_header += ' / ' + d[6]
+    elif d[6] != '':
+        pretty_header += ' ' + d[6]
+    pretty_header += ' (' + d[1] + ')'
 
     # Check if card already exists for character
     crs.execute(
@@ -93,27 +158,50 @@ for l in open(ext_tsv_path, "r", encoding="utf-8"):
     if len(res) > 0:
 
         old = list(res[0])
-        converted_d = d
 
+        # old data: create value strings for better readibility
+        old_string = dict()
+        for data, (idx, name, load_func, _) in zip(old, field_conversion_reverse):
+            old_string[idx] = load_func(data)
+
+        # convert data to json format for modifying to database
+        converted_d = d.copy()
         for data, (idx, name, load_func, _) in zip(d, field_conversion):
             converted_d[idx] = load_func(data)
+
         if old != converted_d:
 
-            print("Old card: ",old)
-            print("Updated card: ", converted_d)
+            logging.info('#### ' + pretty_header)
+
+            logging.info("| Field | Old value | New value |" )
+            logging.info("|---|---|---|" )
+
+            for (idx, field_name, _, _,) in field_conversion:
+                if old[idx] != converted_d[idx] and not (old[idx] is None and converted_d[idx]==''):
+                    logging.info("| %s | %s | %s |" % (field_name.ljust(20), old_string[idx], d[idx]))
+                    total_changes += 1
+
             update_d = converted_d[1:] + [converted_d[0]]
             crs.execute(update_sql, update_d)
             con.commit()
 
     else:
-        converted_d = d
-        for data, (idx, name, load_func, _) in zip(d, field_conversion):
+        logging.info("#### %s (NEW ITEM)" % pretty_header)
+
+        logging.info("| Field | Value |" )
+        logging.info("|---|---|" )
+
+        # convert data to json format for modifying to database
+        converted_d = d.copy()
+        for data, (idx, field_name, load_func, _) in zip(d, field_conversion):
             converted_d[idx] = load_func(data)
-        print("New card: ", converted_d)
+            logging.info("| %s | %s |" % (field_name, d[idx]))
+            total_changes += 1
 
         crs.execute(insert_sql, converted_d)
         con.commit()
 
+logging.info("Processed %d items with total %d changes" % (len(processed_kanji_list), total_changes))
 
 ##################################################################
 print("Reconstructing primitive_of lists..")
@@ -124,36 +212,22 @@ data = crs.fetchall()
 column_names = [description[0] for description in crs.description]
 print("kanji.db column names:", column_names)
 
-poi = column_names.index('primitive_of')
-pi = column_names.index('primitives')
-ci = column_names.index('character')
-ri = column_names.index('radicals')
-fi = column_names.index('frequency_rank') 
-
-
-
-########################## EXPERIMENTAL STUFF ############
-# Keeping this setting on will allow radicals to reference their usage in kanji via primitive_of list
-# instead of restricting it to more complex primitives. 
-# For example 施 has a radical list '方ノ一也' and primitive list '[banner]也'. We would like
-# to back-reference this from radicals viewpoint so that when viewing the page for kanji 方 we could see 
-# that it's used as a component in 施 even though 方 is part of more complex [banner] primitive in that kanji.
-
-# Keeping this disabled for now because it just creates just so new references, 
-# which many of them are not very useful. For example 田　refers to all kanjis that have 由 primitive...
-regard_also_radicals_as_primitives = False   
-# In case the option above is set, we would like to limit this to more common kanjis so that
-# list for 人 doesn't get flooded with extra-rare kanjis
-radical_reference_frequency_cutoff = 2000
-#############################################################
+poi_i = column_names.index('primitive_of')
+pi_i = column_names.index('primitives')
+ci_i = column_names.index('character')
+ri_i = column_names.index('radicals')
+fi_i = column_names.index('frequency_rank')
+hk_i = column_names.index('heisig_keyword6') 
+pk_i = column_names.index('primitive_keywords') 
+m_i = column_names.index('meanings') 
 
 primitive_of_dict = dict()
 
 # Create a lookup table for primitives that are being used by kanjis or other primitives
 for row in data:
-    character = row[ci]
-    primitives = custom_list(row[pi])
-    frequency_rank = row[fi]
+    character = row[ci_i]
+    primitives = custom_list(row[pi_i])
+    frequency_rank = row[fi_i]
 
     for p in primitives:
         if p not in primitive_of_dict:
@@ -161,39 +235,40 @@ for row in data:
         if p != character:
             primitive_of_dict[p] += character
 
-    # Experimental setting, disabled for now..
-    if regard_also_radicals_as_primitives and frequency_rank < radical_reference_frequency_cutoff:
-        radicals = list(row[ri])
-        for r in radicals:
-            if r not in primitive_of_dict:
-                primitive_of_dict[r] = ""
-            if r != character:
-                primitive_of_dict[r] += character
 
 # Calculate missing primitive_of references
+
+logging.info("# Changes in primitives-of list")
+logging.info("| Kanji | Meaning/Keyword | Added | Removed |")
+logging.info("|---|---|---|---|")
 for row in data:
-    character = row[ci]
-    orig_primitive_of = custom_list(row[poi])
+    character = row[ci_i]
+    orig_primitive_of = custom_list(row[poi_i])
     orig_primitive_of_set = set(orig_primitive_of)
     if character in primitive_of_dict:
         primitive_of_set = set(custom_list(primitive_of_dict[character]))
     else:
         primitive_of_set = set()
 
+    # extract the best representation for the kanji/primitive name
+    if row[hk_i] is not None and row[hk_i] != '':
+        name = row[hk_i]
+    elif row[pk_i] is not None and row[pk_i] != '[]':
+        name = j2c(row[pk_i])
+    elif row[m_i] is not None and row[m_i] != '[]':
+        name = j2c(row[m_i])
+    else:
+        name = ""
+
     if primitive_of_set != orig_primitive_of_set:
-        print(character,"\t","Existing refs:",len(orig_primitive_of_set),
-              "Missing from updated list:",orig_primitive_of_set - primitive_of_set,
-              "Missing from existing list:",primitive_of_set-orig_primitive_of_set) 
 
-        if primitive_of_set > orig_primitive_of_set:
-            #print(*row)
-            new_primitive_of_set = primitive_of_set | orig_primitive_of_set
-            print("\tUpdated primitives_of:", new_primitive_of_set)
-            new_data = [''.join(new_primitive_of_set), character]
-            crs.execute(update_prim_of_sql, new_data)
+        added = multiLine(list(primitive_of_set-orig_primitive_of_set),10)
+        removed = multiLine(list(orig_primitive_of_set - primitive_of_set),10)
+        logging.info('|' + character + " | " + name + " | " + added + " | " + removed + ' |')
+        new_primitive_of_set = primitive_of_set
+        new_data = [''.join(new_primitive_of_set), character]
+        crs.execute(update_prim_of_sql, new_data)
 
-            con.commit()
-        else:
-            print("\tLet's not remove any references for now..")
+        con.commit()
 
 con.close()
